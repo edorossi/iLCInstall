a5babc2d875ff3ad3ffc2424045105901660129c
jaarling <jan-hendrik.arling@cern.ch>
2019-04-30 12:01:39 +0200

    CLEANUP: adapt init of ilcsoft modules

--- ilcsoft/__init__.py
+++ ilcsoft/__init__.py
@@ -13,13 +13,13 @@ from lcio import LCIO
 from lccd import LCCD
 from gear import GEAR
 from raida import RAIDA
-from ced import CED
-from kaltest import KalTest, KalDet
+#from ced import CED
+#from kaltest import KalTest, KalDet
 from pathfinder import PathFinder
-from bbq import BBQ
+#from bbq import BBQ
 from gbl import GBL
 
-from ddkaltest import DDKalTest
+#from ddkaltest import DDKalTest
 
 # marlin & friends
 from marlinpkg import MarlinPKG
@@ -27,53 +27,53 @@ from marlinpkg import ConfigPKG
 from marlin import Marlin
 from marlinutil import MarlinUtil
 from marlinreco import MarlinReco
-from cedviewer import CEDViewer
-from pandoranew import PandoraPFANew
-from pandoranew import PandoraAnalysis
-from pandoranew import MarlinPandora
-from lcfivertex import LCFIVertex
+#from cedviewer import CEDViewer
+#from pandoranew import PandoraPFANew
+#from pandoranew import PandoraAnalysis
+#from pandoranew import MarlinPandora
+#from lcfivertex import LCFIVertex
 from eutelescope import Eutelescope
 from overlay import Overlay
-from marlintpc import MarlinTPC
-from ckfit import CKFit
-from fastjet import FastJet, FastJetClustering
-from marlintrk import MarlinTrk
-from kitrack import KiTrack, KiTrackMarlin
+#from marlintpc import MarlinTPC
+#from ckfit import CKFit
+#from fastjet import FastJet, FastJetClustering
+#from marlintrk import MarlinTrk
+#from kitrack import KiTrack, KiTrackMarlin
 #from ddmarlinpandora import DDMarlinPandora 
 
-#slic et al
-from gdml import GDML
-from ddsegmentation import DDSegmentation # standalone DDSegmentation install
+##slic et al
+#from gdml import GDML
+#from ddsegmentation import DDSegmentation # standalone DDSegmentation install
 from lcdd import LCDD
-from slic import SLIC
-from slicpandora import SlicPandora
+#from slic import SLIC
+#from slicpandora import SlicPandora
 
 #aida
-from dd4hep import DD4hep 
-from dd4hep_examples import DD4hepExamples
+#from dd4hep import DD4hep 
+#from dd4hep_examples import DD4hepExamples
  
-from lcgeo import lcgeo
+#from lcgeo import lcgeo
 
-from aidaTT import aidaTT 
+#from aidaTT import aidaTT 
 
 # simtools
 #from simtoolsmaker import SimToolsMaker
-from simtools import *  # modules defined in simtools/__init__.py
+#from simtools import *  # modules defined in simtools/__init__.py
 
 # cmake
 from cmake import CMake
 
 # external (with install support)
-from druid import Druid
-from garlic import Garlic
-from mokka import Mokka
+#from druid import Druid
+#from garlic import Garlic
+#from mokka import Mokka
 from conddbmysql import CondDBMySQL
 from cernlib import CERNLIB
 from clhep import CLHEP
-from heppdt import HepPDT
+#from heppdt import HepPDT
 from gsl import GSL
 from xercesc import XercesC
-from heppdt import HepPDT
+#from heppdt import HepPDT
 from qt import QT
 from dcap import dcap
 
